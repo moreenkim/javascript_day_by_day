764ee636120b377dad782e7a3cc035043ddd4024
moreenkim <moreenkim36@gmail.com>
2020-04-01 11:03:31 +0300

    Return values using the return function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,12 +3,12 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
     const result = num1 + num2; 
-    alert('The result is' + result);
+    // alert('The result is' + result);
+    return result;
 }
 
-add(1, 2);
+currentResult = add(1, 2);
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
