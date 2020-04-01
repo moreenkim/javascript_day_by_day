2c9793ca100947b748527bd74a1fca584eeacd54
moreenkim <moreenkim36@gmail.com>
2020-04-01 10:48:17 +0300

    Define a function, its usability and how to call a function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,12 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2; 
+    alert('The result is' + result);
+}
+
+add(1, 2);
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
