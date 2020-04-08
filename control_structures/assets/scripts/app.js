6130b39289c45beb8005ae97de91f3619b8b8c40
moreenkim <moreenkim36@gmail.com>
2020-04-08 12:59:26 +0300

    Use IF statements to achieve conditions set

--- control_structures/assets/scripts/app.js
+++ control_structures/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
    const damage = dealMonsterDamage(ATTACK_VALUE);
    currentMonsterHealth -= damage;  
+   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+   currentPlayerHealth -= playerDamage;
+   if (currentMonsterHealth <=0 && currentPlayerHealth > 0) {
+       alert('you won!');
+   } else if (currentPlayerHealth <=0 && currentMonsterHealth > 0) {
+       alert('you lost!');
+   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+       alert('its a draw!');
+   }
 }
 
 attackBtn.addEventListener('click', attackHandler);
