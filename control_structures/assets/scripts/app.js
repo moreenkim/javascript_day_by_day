35b8eaaa47bfe18fa7d4f2b5aa90b3d03799606f
moreenkim <moreenkim36@gmail.com>
2020-04-10 16:27:25 +0300

    Use try-catch method for error handling to cushion errors

--- control_structures/assets/scripts/app.js
+++ control_structures/assets/scripts/app.js
@@ -11,13 +11,27 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt('max life', '100');
 
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 
-if(isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+function getMaxLifeValues(){
+    const enteredValue = prompt('max life', '100');
+
+     const parsedValue = parseInt(enteredValue);
+     if(isNaN(parsedValue) || chosenMaxLife <= 0) {
+         throw {message:'invalid input'};
+     }
+    return parsedValue;
+}
+
+let chosenMaxLife;
+
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
     chosenMaxLife = 100;
+    alert('wrong value, default value used!');
 }
 
 let currentMonsterHealth = chosenMaxLife;
@@ -232,6 +246,11 @@ function printLogHandler(){
     for (let i = 0; i<3; i++) {
         console.log('---------');
     }
+    // let j = 0;
+    // while (j < 3) {
+    //     console.log('------');
+    //     j++;
+    // }
     // for(i = 10; i > 0; i--){
     //     console.log(i);
     // }
