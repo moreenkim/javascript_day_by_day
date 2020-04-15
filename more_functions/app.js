0ca79b869a794048734e10cf4b9c22d583f7b270
moreenkim <moreenkim36@gmail.com>
2020-04-15 12:41:41 +0300

    Add more user interaction functionalities to the functions

--- more_functions/app.js
+++ more_functions/app.js
@@ -14,6 +14,32 @@ person.greet();*/
 
 //console.dir(typeof startGame);
 
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+ const getPlayerChoice = function(){
+     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+     if (
+         selection !== ROCK && 
+         selection !== PAPER && 
+         selection !== SCISSORS
+        ) {
+            alert(`Invalid choice! ${DEFAULT_USER_CHOICE} is default`);
+            return DEFAULT_USER_CHOICE;
+        }
+        return selection;             
+};
+
 startGameBtn.addEventListener('click', function(){
+    if (gameIsRunning){
+        return; 
+    }
+    gameIsRunning = true;
     console.log('game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 });
