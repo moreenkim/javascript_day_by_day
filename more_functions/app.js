6c9eed6ed3bd812002e7cc3a06c09abbab5a2055
moreenkim <moreenkim36@gmail.com>
2020-04-15 13:49:02 +0300

    Show the use of arrow functions and incorpotare ternary expressions

--- more_functions/app.js
+++ more_functions/app.js
@@ -24,7 +24,7 @@ const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
- const getPlayerChoice = function(){
+ const getPlayerChoice = () => {
      const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
      if (
          selection !== ROCK && 
@@ -37,7 +37,7 @@ let gameIsRunning = false;
         return selection;             
 };
 
-const getComputerChoice = function(){
+const getComputerChoice = () => {
     const randomValue = Math.random();
     if(randomValue < 0.34) {
         return ROCK;
@@ -48,21 +48,29 @@ const getComputerChoice = function(){
     }
 };
 
-const getWinner = function(cChoice, pChoice) {
-    if( cChoice === pChoice) {
-        return RESULT_DRAW;
-    }else if (
-        cChoice ===ROCK && pChoice === PAPER || 
-        cChoice === PAPER && pChoice === SCISSORS ||
-        cChoice === SCISSORS && pChoice === ROCK
-    ) {
-        return RESULT_PLAYER_WINS;
-    } else {
-        return RESULT_COMPUTER_WINS;
-    }
+const getWinner = (cChoice, pChoice) => {  //arrow function =>
+    return cChoice === pChoice //ternary expression in place of if else if
+        ? RESULT_DRAW 
+        : (cChoice ===ROCK && pChoice === PAPER || 
+          cChoice === PAPER && pChoice === SCISSORS ||
+          cChoice === SCISSORS && pChoice === ROCK) 
+        ? RESULT_PLAYER_WINS 
+        : RESULT_COMPUTER_WINS;
+
+       // if( cChoice === pChoice) { 
+    //     return RESULT_DRAW;
+    // }else if (
+    //     cChoice ===ROCK && pChoice === PAPER || 
+    //     cChoice === PAPER && pChoice === SCISSORS ||
+    //     cChoice === SCISSORS && pChoice === ROCK
+    // ) {
+    //     return RESULT_PLAYER_WINS;
+    // } else {
+    //     return RESULT_COMPUTER_WINS;
+    // }
 };
 
-startGameBtn.addEventListener('click', function(){
+startGameBtn.addEventListener('click', () => {
     if (gameIsRunning){
         return; 
     }
