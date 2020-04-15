b6b6c3319c9cedb233ad8a97e0679cc476bf83d5
moreenkim <moreenkim36@gmail.com>
2020-04-15 13:12:40 +0300

    Implement the core game functions

--- more_functions/app.js
+++ more_functions/app.js
@@ -18,6 +18,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false;
 
@@ -34,12 +37,39 @@ let gameIsRunning = false;
         return selection;             
 };
 
+const getComputerChoice = function(){
+    const randomValue = Math.random();
+    if(randomValue < 0.34) {
+        return ROCK;
+    } else if (randomValue < 0.67){
+        return PAPER;
+    } else {
+        return SCISSORS;
+    }
+};
+
+const getWinner = function(cChoice, pChoice) {
+    if( cChoice === pChoice) {
+        return RESULT_DRAW;
+    }else if (
+        cChoice ===ROCK && pChoice === PAPER || 
+        cChoice === PAPER && pChoice === SCISSORS ||
+        cChoice === SCISSORS && pChoice === ROCK
+    ) {
+        return RESULT_PLAYER_WINS;
+    } else {
+        return RESULT_COMPUTER_WINS;
+    }
+};
+
 startGameBtn.addEventListener('click', function(){
     if (gameIsRunning){
         return; 
     }
     gameIsRunning = true;
     console.log('game is starting...');
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    const ComputerChoice = getComputerChoice();
+    const winner = getWinner(ComputerChoice, playerChoice);
+    console.log(winner);
 });
