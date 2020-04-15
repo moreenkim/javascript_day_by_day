2258f231fed4a53c367128c6291b018d3951059e
moreenkim <moreenkim36@gmail.com>
2020-04-15 12:14:56 +0300

    Demonstrate the use of anonymous functions

--- more_functions/app.js
+++ more_functions/app.js
@@ -1,9 +1,9 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function startGame() {
-    //const start = function() {}; anonymous function
+/*const start = function startGame() {
+        //const start = function() {}; anonymous function
  console.log('game is starting...');
-};
+};*/
 
 /*const person = {                  //function in an object or METHOD
 //     greet: function person() {
@@ -14,4 +14,6 @@ person.greet();*/
 
 //console.dir(typeof startGame);
 
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function(){
+    console.log('game is starting...');
+});
