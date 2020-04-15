8832deca40d92933ed1b66c8dc1de46cda3d898b
moreenkim <moreenkim36@gmail.com>
2020-04-15 11:59:11 +0300

    Create function expressions inside of variables

--- more_functions/app.js
+++ more_functions/app.js
@@ -1,16 +1,17 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+const start = function startGame() {
+    //const start = function() {}; anonymous function
  console.log('game is starting...');
-}
+};
 
-// const person = {                  //function in an object or METHOD
+/*const person = {                  //function in an object or METHOD
 //     greet: function person() {
-//         console.log('hi there!!')
+//     console.log('hi there!!')
 //     }
 // };
-// person.greet();
+person.greet();*/
 
-console.dir(typeof startGame);
+//console.dir(typeof startGame);
 
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
