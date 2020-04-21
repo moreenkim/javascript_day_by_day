a67cc36c0586be0471c47e02d354aecc64addb0b
moreenkim <moreenkim36@gmail.com>
2020-04-21 16:02:06 +0300

    Create add button functionality while prompting the user input

--- DOM_functions/assets/scripts/app.js
+++ DOM_functions/assets/scripts/app.js
@@ -4,6 +4,9 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const addMovieButton = document.querySelector('.btn--success');
+const userInputs = addMovieModal.querySelectorAll('input');
+//const userInputs = addMovieModal.getElementsByTagName('input');
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
@@ -14,15 +17,32 @@ const toggleMovieModal = () => {
     toggleBackdrop('');
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if(
+       titleValue.trim() === '' ||
+       imageUrlValue.trim() === '' ||
+       ratingValue.trim() === '' ||
+       +ratingValue < 1 ||
+       +ratingValue > 5
+       ) {
+           alert('please enter values between 1-5')
+       }
 };
 
 const backdropClickHandler = () =>{
     toggleMovieModal();
-}
+};
 
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click' , cancelAddMovie);
+cancelAddMovieButton.addEventListener('click' , cancelAddMovieHandler);
+addMovieButton.addEventListener('click', addMovieHandler);
