b5b607bc431b17ff10647e60e0cd89d11314ba7f
moreenkim <moreenkim36@gmail.com>
2020-04-23 18:14:40 +0300

    Demonstrate the use of the foreach() method inplace of the for loop'

--- Arrays_Iterables/app.js
+++ Arrays_Iterables/app.js
@@ -44,26 +44,40 @@ for(const data of analytics) {
 // hobbies.splice(0, 1); //remove element from array
 // console.log(hobbies);
 
-const testResults = [1, 5.2, -6.2, 10, 15];
-//const storedResults = testResults.slice(0, 2);
-const storedResults = testResults.concat([3.99, 2]);
+// const testResults = [1, 5.2, -6.2, 10, 15];
+// //const storedResults = testResults.slice(0, 2);
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(7);
+// testResults.push(7);
 
-console.log(storedResults, testResults);
-console.log(testResults.indexOf(5.2));
-//console.log(testResults.lastIndexOf(5.2));  //start searching from the right of the array
-console.log(testResults.includes(5.2));
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(5.2));
+// //console.log(testResults.lastIndexOf(5.2));  //start searching from the right of the array
+// console.log(testResults.includes(5.2));
 
-const personData = [{name:'max'}, {name:'manuel'}];
-console.log(personData.indexOf({name:'max'}));
+// const personData = [{name:'max'}, {name:'manuel'}];
+// console.log(personData.indexOf({name:'max'}));
 
-const manuel = personData.find((person, index, persons) => {
-    return person.name === 'manuel';
-});
-console.log(manuel);
+// const manuel = personData.find((person, index, persons) => {
+//     return person.name === 'manuel';
+// });
+// console.log(manuel);
 
-const maxIndex = personData.findIndex((person, index, persons) => {
-    return person.name === 'max';
-});
-console.log(maxIndex);
+// const maxIndex = personData.findIndex((person, index, persons) => {
+//     return person.name === 'max';
+// });
+// console.log(maxIndex);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+for(const price of prices){
+    taxAdjustedPrices.push(price * (1+tax));
+}
+
+prices.forEach((price, idx, prices) => {
+    const priceObject = {index: idx, taxAdjustedPrice: price * (1+tax)}
+    taxAdjustedPrices.push(priceObject);
+})
+console.log(taxAdjustedPrices);
