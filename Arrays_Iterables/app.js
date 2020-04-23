965678011194b4a6d5b5507e272b28dc61b3d9a8
moreenkim <moreenkim36@gmail.com>
2020-04-23 15:07:57 +0300

    Demonstrate what elements can be stored in an array

--- Arrays_Iterables/app.js
+++ Arrays_Iterables/app.js
@@ -8,11 +8,22 @@ console.log(numbers);
 // const yetMoreNumbers = Array.of(1, 2);
 // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
+const listItems = document.querySelectorAll('li'); //will cr8 nodelist
 console.log(listItems);
 
-const listItemsArray = Array.from(listItems);
+const listItemsArray = Array.from(listItems); //will cr8 real array
 console.log(listItemsArray);
 
 const moreNumbers = Array.from('hiii'); //convert iterable and array like to an array
 console.log(moreNumbers);
+
+const hobbies = ['cooking', 'sports'];  //what can be stored in arrays
+const personalData = [30, 'max', {moreData: []}];
+
+const analytics = [[1, 1.5], [-5, 4.2]]; //multidimentional or nested arrays
+
+for(const data of analytics) {
+    for(const datapoint of data) {
+        console.log(datapoint);
+    }
+}
