9e5748f6de08ca28f60df3cb54a5553c28fa5587
moreenkim <moreenkim36@gmail.com>
2020-04-23 12:19:38 +0300

    Create boiler plate code

---
+++ Arrays_Iterables/app.js
@@ -0,0 +1,15 @@
+const numbers = [1, 2, 3]; //most common way
+console.log(numbers);
+
+// //const moreNumbers = new Array('hi', 'there'); // []
+// const moreNumbers = new Array(3);//will produce empty*3//(1, 5);
+// console.log(moreNumbers);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
+
+const moreNumbers = Array.from('hiii'); //convert iterable and array like to an array
+console.log(moreNumbers);
