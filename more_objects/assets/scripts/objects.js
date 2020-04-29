bb66a0dd95924aab69cb7f56b482125e54deb5a2
moreenkim <moreenkim36@gmail.com>
2020-04-29 15:23:56 +0300

    Use for in loops to update user output

--- more_objects/assets/scripts/objects.js
+++ more_objects/assets/scripts/objects.js
@@ -1,10 +1,11 @@
 const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
-const newMovie = [];
+const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
   const movieList = document.getElementById('movie-list');
+  //debugger;
  
   if (movies.length === 0){
     movieList.classList.remove('visible');
@@ -14,11 +15,25 @@ const renderMovies = () => {
   }
   movieList.innerHTML= '';
 
-  movies.foreach((movie) => {
-    const movieEl = document.createElement('LI');
-    movieEl.textContent = movie.info.title;
+  const filteredMovies = !filter 
+    ? movies 
+    : movies.filter(movies => movie.info.title.includes(filter));
+
+
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement('li'); //this becomes an object
+    //debugger;
+    let text = movie.info.title  + ' - ';  //let text = movie.info['title'] output key value in an object
+    for (const key in movie.info){
+      if(key !==  'title') {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    //movieEl.textContent = movie.info.title;
+    movieEl.textContent = text;
     movieList.append(movieEl);
-  })
+  });
 };
 
 const addMovieHandler = () => {
@@ -47,4 +62,10 @@ const addMovieHandler = () => {
 
 };
 
+// const searchMovieHandler = () => {
+//   const filterTerm = document.getElementById('filter-value').value;
+//   renderMovies(filterTerm);
+// }
+
 addMovieBtn.addEventListener('click', addMovieHandler);
+//searchBtn.addEventListener('click', searchMovieHandler);
